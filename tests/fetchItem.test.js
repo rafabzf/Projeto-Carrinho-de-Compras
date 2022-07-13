e896fd986bdc95eae5abcfb26bd299d03d922ae8
Rafael <rafinhabzf@gmail.com>
2022-07-13 16:41:08 -0300

    Resolvendo problema GitHub

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,19 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqu
-  fail('Teste vazio');
+  it('Testa se é uma função', () => {
+    const actual = typeof fetchItem;
+    const expected = 'function';
+
+    expect(actual).toBe(expected);
+  });
+
+  it('Testa se ao chamar a função com o argumento "MLB1615760527", ela utiliza o endpoint correto', async () => {
+    const actual = await fetchItem('MLB1615760527');
+    const expected = 'https://api.mercadolibre.com/items/MLB1615760527'
+
+    expect(actual).toBe(expected);
+  });
+
+  it()
 });
